e86049e0c2f845293490e7ab56dc7a8853bf48a0
Alejandro <aldrolin05@gmail.com>
2026-04-16 19:55:26 -0500

    primer commit

--- src/main/java/com/example/demo/repository/MascotaRepository.java
+++ src/main/java/com/example/demo/repository/MascotaRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public class MascotaRepository {
+import com.example.demo.entity.Mascota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MascotaRepository extends JpaRepository<Mascota, Integer> {
 }
